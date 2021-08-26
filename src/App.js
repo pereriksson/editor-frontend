e0cff8a6a4a20bfa2281e31c348db3cdfe7344ed
Per Eriksson <per@interapp.se>
2021-08-26 20:22:37 +0200

    added save button

--- src/App.js
+++ src/App.js
@@ -23,11 +23,20 @@ function App() {
               'searchreplace visualblocks code fullscreen',
               'insertdatetime media table paste code help wordcount'
             ],
-            toolbar: 'undo redo | formatselect | ' +
+            toolbar: 'save undo redo | formatselect | ' +
                 'bold italic backcolor | alignleft aligncenter ' +
                 'alignright alignjustify | bullist numlist outdent indent | ' +
                 'removeformat | help',
-            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+            setup: function (editor) {
+                editor.ui.registry.addButton('save', {
+                    text: 'Save',
+                    icon: 'save',
+                    onAction: function (_) {
+                        log();
+                    }
+                });
+            }
           }}
         />
     </div>
