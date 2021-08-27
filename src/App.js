dafa3e2ff9494f5c46a90fc6d4f6f72ae48fd68d
Per Eriksson <per@interapp.se>
2021-08-27 11:44:08 +0200

    added ability to open documents

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useRef} from "react";
 import { Editor } from '@tinymce/tinymce-react';
 import './App.css';
+import {getTinymce} from "@tinymce/tinymce-react/lib/es2015/main/ts/TinyMCE";
 
 function App() {
   const editorRef = useRef(null);
@@ -10,11 +11,13 @@ function App() {
     }
   };
 
+  const state = {};
+  state.test = "test";
+
   return (
     <div className="App">
         <Editor
           onInit={(evt, editor) => editorRef.current = editor}
-          initialValue="<p>This is the initial content of the editor.</p>"
           init={{
             height: 500,
             menubar: false,
@@ -23,7 +26,7 @@ function App() {
               'searchreplace visualblocks code fullscreen',
               'insertdatetime media table paste code help wordcount'
             ],
-            toolbar: 'save undo redo | formatselect | ' +
+            toolbar: 'save open undo redo | formatselect | ' +
                 'bold italic backcolor | alignleft aligncenter ' +
                 'alignright alignjustify | bullist numlist outdent indent | ' +
                 'removeformat | help',
@@ -36,6 +39,65 @@ function App() {
                         log();
                     }
                 });
+
+                editor.ui.registry.addButton('open', {
+                    text: 'Open',
+                    icon: 'upload',
+                    onAction: async function (_) {
+                        const tinymce = getTinymce();
+
+                        state.documents = await fetch("http://localhost:1337/v1/documents")
+                            .then(res => res.json());
+
+                        let html = "";
+                        let checked = "checked";
+
+                        state.documents.forEach(d => {
+                            html += `
+                                <li>
+                                    <input type="radio" ${checked} id="document_${d._id}" name="documentId" value="${d._id}"/>
+                                    <label for="document_${d._id}">${d.name}</label>
+                                </li>
+                            `;
+
+                            checked = "";
+                        });
+                        html = `
+                            <form name="openDocumentForm">
+                            <p>Select the document to open:</p>
+                            <ul>
+                                ${html}
+                            </ul>
+                            </form>
+                        `;
+
+                        tinymce.activeEditor.windowManager.open({
+                            title: "Open",
+                            body: {
+                                type: "panel",
+                                items: [
+                                    {
+                                        type: 'htmlpanel',
+                                        html
+                                    }
+                                ]
+                            },
+                            buttons: [
+                                {
+                                    type: 'submit',
+                                    text: 'Open',
+                                    primary: true
+                                }
+                            ],
+                            onSubmit: function(dialogApi) {
+                                state.currentDocumentId = document.querySelector("input[name='documentId']:checked").value;
+                                const currentDocument = state.documents.find(d => d._id === state.currentDocumentId);
+                                editorRef.current.setContent(currentDocument.contents);
+                                dialogApi.close();
+                            }
+                        })
+                    }
+                })
             }
           }}
         />
